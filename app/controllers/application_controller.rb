261ef25e9ddf837174b9a61a7135a1e62beb589e
Egor Arbuzov <egorarbuzov@gmail.com>
2025-03-23 19:49:09 +0300

    add modal user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale, :set_render_cart, :initialize_cart, :search_field
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   private
 
   def see_count_cart
